76a993da598647f6ac9b4c3365c9ffea9b7d6958
Igor Sedura <igorsedura@gmail.com>
2022-12-16 21:55:47 +0200

    add form phone number

---
+++ src/components/FormAddPhone/FormAddPhone.jsx
@@ -0,0 +1,33 @@
+import { Component } from 'react';
+
+export class FormAddPhone extends Component {
+  render() {
+    return (
+      <form>
+        <div>
+          <label>Name</label>
+          <input
+            placeholder="Введіть ім'я"
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </div>
+        <div>
+          <label>Number</label>
+          <input
+            placeholder="Введіть номер"
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+        </div>
+        <button>Add to contact</button>
+      </form>
+    );
+  }
+}
